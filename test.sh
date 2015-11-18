d7f682702ba7e0bd62abb4c34ca4eea3518ac8e0
pollseed <cb.so.devope+0001@gmail.com>
2015-11-19 01:14:23 +0900

    #37 add testshell

--- test.sh
+++ test.sh
@@ -1,1 +1,18 @@
-npm start
+#!/bin/bash
+
+readonly PORT=3000
+readonly HTTP_STATUS='%{http_code}\n'
+readonly SUCCESS=200
+
+function pm2_start() {
+  pm2 start bin/www $2>/dev/null
+  sleep 1
+}
+
+function error_check() {
+  curl -LI --user 'dream:hackers' localhost:${PORT} -o /dev/null -w ${HTTP_STATUS} -s | grep ${SUCCESS} $2>/dev/null
+}
+
+pm2_start
+error_check
+echo $?
