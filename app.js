3d718d185bac6a8165c858cbca611f177f21478d
pollseed <cb.so.devope+0001@gmail.com>
2015-11-23 16:01:58 +0900

    #33 ref

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
 var basicAuth = require('basic-auth-connect');
+var favicon = require('serve-favicon');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -23,8 +24,7 @@ var Sample = model.Sample;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
